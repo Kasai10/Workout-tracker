be29e071525c8b0b301d1b9c49498100e89ae1f5
Kasai10 <simobeneckert@gmail.com>
2025-12-05 15:43:10 +0100

    little change

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 8050
 
 # Run the app
-CMD ["python", "app.py"]
+CMD ["python", "dash_app/app.py"]
--- dash_app/app.py
+++ dash_app/app.py
@@ -689,4 +689,4 @@ def delete_target_row(previous, current):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False,  host="0.0.0.0", port=8050)
